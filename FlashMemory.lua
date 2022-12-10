b847aee57e4c66a4235be639b40041f6086b2327
xVice <47506182+xVice@users.noreply.github.com>
2022-12-10 19:36:10 +0100

    Update FlashMemory.lua

--- FlashMemory.lua
+++ FlashMemory.lua
@@ -3,9 +3,9 @@ local Mem:FlashMemory = gdt.FlashMemory0
 
 -- update function is repeated every time tick
 function update()
-		Mem:Save({420,590,220})
-		memTable = Mem:Load()
-		for i, num in ipairs(memTable) do
-			log(tostring(num))
-		end
+	Mem:Save({420,590,220})
+	memTable = Mem:Load()
+	for i, num in ipairs(memTable) do
+		log(tostring(num))
+	end
 end
